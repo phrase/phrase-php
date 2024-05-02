fd2f143a875b569585f3ae90818b24f4c1995a49
Phrase <support@phrase.com>
2024-05-02 15:11:13 +0000

    Deploying from  phrase/openapi@3135099c

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -86,7 +86,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/2.1.0/PHP';
+    protected $userAgent = 'OpenAPI-Generator/2.1.1/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -395,7 +395,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 2.0.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 2.1.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.1.1' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
